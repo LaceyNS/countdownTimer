45b7f2f4925e6bd679da4baa3084f4b4ae2b6fa8
Lacey Savage <lacey.savage@gmail.com>
2021-09-01 17:41:40 -0400

    finish lesson

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+    //clear any existing timers
+    clearInterval(countdown);
+
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
@@ -40,3 +44,19 @@ function displayEndTime(timestamp) {
     endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes}`;
 
 }
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+    console.log(seconds);
+}
+
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit',function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+})
