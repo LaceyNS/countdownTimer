4f3bd0c9dd1f8c3f2e2aae76a0b3983e48f1b915
Lacey Savage <lacey.savage@gmail.com>
2021-09-01 17:32:44 -0400

    add return time under the timer

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let countdown;
-const timerDisplay = document.querySelector('.display__time-left')
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -23,5 +25,18 @@ function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
     timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    //if you want a 24 hour clock
+    // endTime.textContent = `Be Back At ${hour}:${minutes}`;
+
+    //if you want a 12 hour clock
+    endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 : hour}:${minutes}`;
+
 }
